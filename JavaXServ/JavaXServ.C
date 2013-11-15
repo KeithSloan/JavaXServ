dd5b46308a628f2e29c67abc5da0f596d3b50370
Keith Sloan <keith@sloan-home.co.uk>
2013-11-15 18:19:20 +0000

    Forgot git add . on previous commit

--- JavaXServ/JavaXServ.C
+++ JavaXServ/JavaXServ.C
@@ -14,6 +14,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with JavaXServ.  If not, see <http://www.gnu.org/licenses/>.
+  
 
 #include <iostream>
 #include <cstring>
--- www/XServ/Xrequest.java
+++ www/XServ/Xrequest.java
@@ -14,8 +14,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with JavaXServ.  If not, see <http://www.gnu.org/licenses/>.
-
-
+  
 import java.awt.*;
 import java.awt.image.*;
 
